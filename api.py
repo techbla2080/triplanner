bb1d25991e3a217dcab534ec2f4cc3cb6eef1369
pranav <pranav@techbla.in>
2024-12-05 23:05:08 +0530

    Updated CORS config for Vercel deployment

--- api.py
+++ api.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 CORS(app, 
      resources={
          r"/*": {
-             "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
+             "origins": ["http://localhost:3000", "http://127.0.0.1:3000", "https://quipitnextjs.vercel.app"],
              "methods": ["GET", "POST", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization"],
              "supports_credentials": True
